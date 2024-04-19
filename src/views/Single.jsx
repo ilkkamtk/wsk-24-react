077cc453d5eabcd42c88fd3330bc4f8edf2b4a4f
Juha Tauriainen <juha@bin.fi>
2024-04-19 12:52:26 +0300

    Add upload example, separate example data model to reusable file

--- src/views/Single.jsx
+++ src/views/Single.jsx
@@ -1,15 +1,24 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation, useNavigate, useParams } from "react-router-dom"
 import SingleView from "../components/SingleView"
+import mediaArray from "../data/media"
 
 const Single = () => {
   const location = useLocation()
   const navigate = useNavigate()
+  const params = useParams()
 
+  const id = Number(params.id)
+
+  let item;
   if (!location.state?.item) {
-    return null
+    item = mediaArray.find(({media_id}) => Number(media_id) === id)
+  } else {
+    item = location.state.item
   }
 
-  const { item } = location.state
+  if(!item) {
+    return <>Media not found</>
+  }
 
   return <SingleView
     selectedItem={item}
