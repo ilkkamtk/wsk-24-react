ed09576e83baf3a965f1e8076378adf015d37999
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-04-22 11:25:08 +0300

    ass 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Home from './views/Home';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import { Profile } from './views/Profile';
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import {Profile} from './views/Profile';
 import Upload from './views/Upload';
 import Layout from './views/Layout';
 import Single from './views/Single';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
