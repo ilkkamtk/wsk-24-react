e1b38acdcbb20bf0b7b8e0b4c0ada781e5a62907
Juha Tauriainen <juha@bin.fi>
2024-04-24 14:12:07 +0300

    Add autologin, logout and protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,30 @@ import Layout from './views/Layout';
 import Single from './views/Single';
 import Login from './views/Login';
 import { UserProvider } from './contexts/UserContext';
+import HandleAutoLogin from './components/HandleAutoLogin';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
+  const basename = import.meta.env.BASE_URL
+  console.log("basename", basename)
+
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router basename={basename}>
       <UserProvider>
+        <HandleAutoLogin />
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/upload" element={<Upload />} />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } />
+            <Route path="/upload" element={
+              <ProtectedRoute>
+                <Upload />
+              </ProtectedRoute>
+            } />
             <Route path="/media/:id" element={<Single />} />
             <Route path="/login" element={<Login />} />
           </Route>
--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,44 +1,15 @@
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { useUserContext } from '../contexts/UserContext';
-import { useUser } from '../hooks/apiHooks';
 import UserData from '../components/UserData';
 
-export const Profile = () => {
-  const { setUser } = useUserContext();
-  const { getUserByToken } = useUser();
+export const Profile = () => (
+  <div>
+    <h2 className="text-2xl font-bold">Tämä on minun profiilisivu</h2>
 
-  const getUser = async () => {
-    try {
-      const token = localStorage.getItem('token');
-      const userData = await getUserByToken(token);
-      setUser(userData.user);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
-
-  useEffect(() => {
-    getUser();
-  }, []);
-
-  return (
+    <p>
+      <Link to="/">Navigoi takaisin etusivulle</Link>
+    </p>
     <div>
-      <h2 className="text-2xl font-bold">Tämä on minun profiilisivu</h2>
-
-      <p>
-        <Link to="/">Navigoi takaisin etusivulle</Link>
-      </p>
-      <div>
-        <UserData />
-      </div>
+      <UserData />
     </div>
-  );
-};
-
-
-
-
-
-
-
+  </div>
+);
